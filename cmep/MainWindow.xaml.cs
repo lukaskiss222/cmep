8888f31f10facaa5d82b8b97a40881a03ead84b5
Lukas Kiss <lukaskiss222@gmail.com>
2018-08-25 13:14:41 +0200

    Setuping database and using ChoETL to parse data.

--- cmep/MainWindow.xaml.cs
+++ cmep/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChoETL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,15 @@ namespace cmep
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var line in new ChoCSVReader("TRAFO-ReadReport.20180419123715.csv"))
+            {
+            }
+                
+            MessageBox.Show("asdasd");
         }
     }
 }
